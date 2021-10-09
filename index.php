8e6c0bfcba5b3e3cbbcf6ec39acde4c0ba61f608
Goran <dragor.dev@gmail.com>
2021-10-09 18:39:46 +0200

    commit...

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+    session_start();
+    require_once 'system/Application.php';
+    
+    require_once 'system/DB.php';
+    require_once 'system/ORM.php';
+    require_once 'system/Collection.php';
+    
+    require_once 'system/Helper.php';
+    require_once 'system/ImportFiles.php';
+    
+    $path = rtrim('http://' . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'], 'index.php');
+
+    DEFINE("BASE_URL", $path);
+    DEFINE("BASE_PATH", getcwd());
+    
+    $uri  = isset($_GET['uri']) ? $_GET['uri'] : '';
+    $data = isset($data) ? $data : '';
+    
+    $app = new Application();
+    
+    $app->citajRutu($uri, $data);
+    
+?>
